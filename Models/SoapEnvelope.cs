b88fec386c2b86c0680d6fdb2a1bd25d44d8153e
Антон Гридюшко <gridjushko@pit.protei.ru>
2024-09-19 16:40:58 +0300

    Add project files.

---
+++ Models/SoapEnvelope.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Serialization;
+
+namespace SocketConsole.Models
+{
+    [XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class SoapEnvelope
+    {
+        [XmlElement(ElementName = "Body")]
+        public SoapBody Body { get; set; }
+    }
+    public class SoapBody
+    {
+        [XmlElement(ElementName = "card112ChangedRequest", Namespace = "http://www.protei.ru/emergency/integration")]
+        public Card112ChangedRequest Card112ChangedRequest { get; set; }
+    }
+
+    public class Card112ChangedRequest
+    {
+        [XmlElement(ElementName = "globalId")]
+        public string GlobalId { get; set; }
+
+        [XmlElement(ElementName = "nEmergencyCardId")]
+        public int EmergencyCardId { get; set; }
+
+        [XmlElement(ElementName = "dtCreate")]
+        public DateTime DtCreate { get; set; }
+
+        [XmlElement(ElementName = "nCallTypeId")]
+        public int CallTypeId { get; set; }
+
+        [XmlElement(ElementName = "nCardSyntheticState")]
+        public int CardSyntheticState { get; set; }
+
+        [XmlElement(ElementName = "lWithCall")]
+        public int WithCall { get; set; }
+
+        [XmlElement(ElementName = "strCreator")]
+        public string Creator { get; set; }
+
+        [XmlElement(ElementName = "strAddressLevel1", IsNullable = true)]
+        public string AddressLevel1 { get; set; }
+
+        [XmlElement(ElementName = "strIncidentDescription", IsNullable = true)]
+        public string IncidentDescription { get; set; }
+
+        // Добавьте остальные поля, соответствующие вашей XML-структуре
+    }
+}
